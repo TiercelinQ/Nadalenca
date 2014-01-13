9a88011c7b3ad43fc7c360ad5114d761c6280a29
ivori <ivori.koyot@gmail.com>
2014-01-13 17:21:53 +0100

    ControlConcert concert passe beulej

--- Controleur/controlConcert.php
+++ Controleur/controlConcert.php
@@ -2,7 +2,7 @@
 include ("./config.php");
 include("./menu.php"); 
 
-echo 'Concerts de Nadalenca :
+echo 'Concerts future de Nadalenca :
 		<table border = "1">
 			<tr>
 				<td>Titre_Concert</td>
@@ -13,6 +13,32 @@ echo 'Concerts de Nadalenca :
 				<td>Ville</td>';
 
 	$req="SELECT * from concert where dateC>now() order by dateC ASC "; // requête 
+	$res =mysql_query($req); // envoi de la requete
+
+	while ($tuple = mysql_fetch_array($res))
+	{ 
+		echo "<tr>"
+				."<td>".$tuple['titreC']."</td>"
+				."<td>".$tuple['dateC']."</td>"
+				."<td>".$tuple['heureC'].'h'.$tuple['minuteC']."</td>"
+				."<td>".$tuple['lieuC']."</td>"
+				."<td>".$tuple['adresseC']."</td>"
+				."<td>".$tuple['ville']."</td>"	 
+			."</tr>";
+	}
+		echo "</table><br/>";
+		
+		echo 'Concerts passé de Nadalenca :
+		<table border = "1">
+			<tr>
+				<td>Titre_Concert</td>
+				<td>Date</td>
+				<td>Heure</td>
+				<td>Lieu</td>
+				<td>Adresse</td>
+				<td>Ville</td>';
+
+	$req="SELECT * from concert where dateC<now() order by dateC ASC "; // requête 
 	$res =mysql_query($req); // envoi de la requete
 
 	while ($tuple = mysql_fetch_array($res))
