e8f5478ecce4a97e246a5ef3abd57f4cdc5f7786
TiercelinQ <q.tiercelin@gmail.com>
2014-01-13 16:14:27 +0100

    modif concert

--- Controleur/controlConcert.php
+++ Controleur/controlConcert.php
@@ -1,29 +1,23 @@
 <?php
-
+include ("./config.php");
 include("./menu.php"); 
-include ("./config.php"); 
-include ("./Modele/Concert.php");
 
-echo 'Concert de Nadalenca :
+echo 'Concerts de Nadalenca :
 		<table border = "1">
 			<tr>
-				<td>id</td>
 				<td>Titre_Concert</td>
 				<td>Date</td>
 				<td>Heure</td>
 				<td>Lieu</td>
 				<td>Adresse</td>
 				<td>Ville</td>';
-				
-
 
 	$req="SELECT * from concert where dateC>now() order by dateC ASC "; // requete
 	$res =mysql_query($req); // envoi de la requete
 
-	while ($tuple = mysql_fetch_array($res)or die(mysql_error()))
+	while ($tuple = mysql_fetch_array($res))
 	{ 
 		echo "<tr>"
-				."<td>".$tuple['id']."</td>"
 				."<td>".$tuple['titreC']."</td>"
 				."<td>".$tuple['dateC']."</td>"
 				."<td>".$tuple['heureC'].'h'.$tuple['minuteC']."</td>"
@@ -36,3 +30,5 @@ echo 'Concert de Nadalenca :
 		
 		
 		require("./footer.php");
+?>
+
