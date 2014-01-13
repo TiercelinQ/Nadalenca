826ae02faaab95a10389c28d0b06b8b88a9cd20a
TiercelinQ <q.tiercelin@gmail.com>
2014-01-13 09:53:02 +0100

    Nadalenca SQL

---
+++ nadalenca.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Lun 13 Janvier 2014 à 08:51
+-- Version du serveur: 5.6.12-log
+-- Version de PHP: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `nadalenca`
+--
+CREATE DATABASE IF NOT EXISTS `nadalenca` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `nadalenca`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `concert`
+--
+
+CREATE TABLE IF NOT EXISTS `concert` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titreC` varchar(255) NOT NULL,
+  `jourC` varchar(8) NOT NULL,
+  `numJourC` int(2) NOT NULL,
+  `moisC` varchar(10) NOT NULL,
+  `anneeC` int(4) NOT NULL,
+  `heureC` int(2) NOT NULL,
+  `minuteC` int(2) NOT NULL,
+  `lieuC` varchar(255) NOT NULL,
+  `adresseC` varchar(255) NOT NULL,
+  `ville` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `concert`
+--
+
+INSERT INTO `concert` (`id`, `titreC`, `jourC`, `numJourC`, `moisC`, `anneeC`, `heureC`, `minuteC`, `lieuC`, `adresseC`, `ville`) VALUES
+(1, 'Joyeux Noel', 'Lundi', 23, 'Decembre', 2013, 20, 30, 'Mairie de Montpellier', '55 avenue des choux blancs', 'Montpellier');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE IF NOT EXISTS `message` (
+  `idM` int(11) NOT NULL AUTO_INCREMENT,
+  `nomM` varchar(255) NOT NULL,
+  `prenomM` varchar(255) NOT NULL,
+  `emailM` varchar(255) NOT NULL,
+  `sujetM` varchar(255) NOT NULL,
+  `messageM` varchar(255) NOT NULL,
+  `dateM` varchar(30) NOT NULL,
+  PRIMARY KEY (`idM`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `message`
+--
+
+INSERT INTO `message` (`idM`, `nomM`, `prenomM`, `emailM`, `sujetM`, `messageM`, `dateM`) VALUES
+(1, 'pochon', 'quentin', 'q.tiercelin@gmail.com', 'test', 'test', '19-12-13'),
+(2, 'tiercelin', 'quentin', 'q.tiercelin@gmail.com', 'test2', 'test2', '19-12-13'),
+(3, 'bexchauvet', 'olivier', 'q.tiercelin@gmail.com', 'test3', 'test', '19-12-13'),
+(4, 'tiercelin', 'quentin', 'q.tiercelin@gmail.com', 'test3', 'test', '19-12-13');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `statut` varchar(255) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `adresseP` varchar(255) NOT NULL,
+  `codeP` varchar(255) NOT NULL,
+  `ville` varchar(255) NOT NULL,
+  `numtelF` varchar(14) NOT NULL,
+  `numtelM` varchar(14) NOT NULL,
+  `voix` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `email`, `mdp`, `statut`, `admin`, `adresseP`, `codeP`, `ville`, `numtelF`, `numtelM`, `voix`) VALUES
+(1, 'Tiercelin', 'Quentin', 'q.tiercelin@gmail.com', 'licorne', '', 0, '20, avenue du Docteur Pezet', '34090', 'Montpellier', '04.99.23.93.98', '06.50.80.66.77', 'voix'),
+(2, 'Beule', 'JÃ©rÃ©my', 'androalphas@hotmail.fr', 'licorne', '', 0, '', '', '', '', '', 'voix'),
+(3, 'bexchauvet', 'olivier', 'fullofwrath@hotmail.fr', 'licorne', '', 0, '', '', '', '', '', 'voix');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
