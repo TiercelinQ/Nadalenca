dd5f7b3c8735de6cd39e90cb769f99e59033aa2b
pochona <momoamo@hotmail.fr>
2014-01-14 13:02:15 +0100

    Modif pages profils Am
    Modif de beug lors de l'accès a la page Profil sans être connecté.

--- Controleur/controlProfile.php
+++ Controleur/controlProfile.php
@@ -1,16 +1,16 @@
 <?php
 include ("./Modele/Utilisateur.php");
 
-$email = $_SESSION['login']; 
-$profil = Utilisateur::getUtilisateurByEmail($email);
 
 
-if ($profil == null)
-{
-    require ('./Vue/viewErrorProfil.php'); //redirige vers une vue d'erreur
+
+if ((!empty($_SESSION['login'])) || (isset($_SESSION['login']))) {
+	$email = $_SESSION['login']; 
+	$profil = Utilisateur::getUtilisateurByEmail($email);
+    require ('./Vue/viewProfil.php'); //redirige vers une vue d'erreur
 }
 else
 {
-    require ('./Vue/viewProfil.php'); //redirige vers la vue
+    require ('./Vue/viewErrorProfil.php'); //redirige vers la vue
 }
 ?>
--- Vue/viewErrorProfil.php
+++ Vue/viewErrorProfil.php
@@ -5,9 +5,10 @@
    	 	<title>ERREUR</title>
    	 	<link rel="stylesheet" type="text/css" href="../style.css" />
     </head>
-
 	<body>
+<?php include("./menu.php"); ?>	
+
 			<h3>Cette personne n'existe pas !</h3>
    	 		<a href="../index.php">Retour à la page d'accueil</a>
 
-<?php include("../footer.php"); ?>	
+<?php include("./footer.php"); ?>	
