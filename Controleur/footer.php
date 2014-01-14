92378dc08f4e59154378b72380f0aeefa49ff1e1
TiercelinQ <q.tiercelin@gmail.com>
2014-01-14 17:03:58 +0100

    style css suite

--- Controleur/footer.php
+++ Controleur/footer.php
@@ -1,12 +1,10 @@
 	<hr />
-		<div id="footer">
-			<ul>
+			<ul id="footer">
 				<li><a href="#">Qui sommes nous ?</a></li>
 				<li><a href="./controlContact.php">Nous contacter</a></li>
 				<li><a href="#">Plan du site</a></li>
 				<li><a href="#">Liens</a></li>
 			</ul>
-		</div>
 	</div>
 	</body>
 </html>	
