0d66a8f45a5295ece7ed28610cef987e98037ea5
TiercelinQ <q.tiercelin@gmail.com>
2014-01-14 16:49:14 +0100

    footer correction

--- Controleur/footer.php
+++ Controleur/footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 	<hr />
 		<div id="footer">
 			<ul>
@@ -7,17 +6,6 @@
 				<li><a href="#">Plan du site</a></li>
 				<li><a href="#">Liens</a></li>
 			</ul>
-=======
-		<hr />
-			<div id="footer">
-				<ul>
-					<li><a href="#">Qui sommes nous ?</a></li>
-					<li><a href="./contact.php">Nous contacter</a></li>
-					<li><a href="#">Plan du site</a></li>
-					<li><a href="#">Liens</a></li>
-				</ul>
-			</div>
->>>>>>> suite CSS accueil + redirection style.css
 		</div>
 	</div>
 	</body>
