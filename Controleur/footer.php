9e4e695fb55dfbd7c50e78c7b276f57f929db84a
pochona <momoamo@hotmail.fr>
2014-01-15 11:35:48 +0100

    Modif footer Am

--- Controleur/footer.php
+++ Controleur/footer.php
@@ -8,7 +8,7 @@
 				<li><a href="#">Qui sommes nous ?</a></li> | 
 				<li><a href="./controlContact.php">Nous contacter</a></li> | 
 				<li><a href="#">Plan du site</a></li> | 
-				<li><a href="#">Liens</a></li>
+				<li><a href="controlLiens.php">Liens</a></li>
 			</ul>
 <?php 	}
 		else
