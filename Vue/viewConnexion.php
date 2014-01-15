7a3969b02b31c38054d544181c11efe637bad2b7
TiercelinQ <q.tiercelin@gmail.com>
2014-01-15 11:54:47 +0100

    style form firefox

--- Vue/viewConnexion.php
+++ Vue/viewConnexion.php
@@ -1,6 +1,7 @@
+<h1>CONNEXION</h1>
+<hr />
 <form method="POST" action="../Controleur/controlVerifConnexion.php">
 					<fieldset>
-						<legend>Formulaire de connexion</legend>
 							<label>Email : </label><input type="email" name="email" placeholder="email" required>
 							<label>Mot de passe : </label><input type="password" name="mdp" required>
 								<input type="submit" value="Connexion">	<br />
