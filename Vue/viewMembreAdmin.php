0a45ba36ccb8d8f83b23fdd77918a61c44ea6a73
TiercelinQ <q.tiercelin@gmail.com>
2014-01-15 18:40:01 +0100

    Design suite admin membre

--- Vue/viewMembreAdmin.php
+++ Vue/viewMembreAdmin.php
@@ -1,29 +1,32 @@
-Membres de Nadalenca :
-		<table border = "1">
-			<tr>
-				<td>id</td>
-				<td>Nom</td>
-				<td>Prénom</td>
-				<td>Adresse Email</td>
-				<td>Admin</td>
-				<td>ChangerStatut</td>
-				<td>Supprimer membre</td>
+<h1>Annuaire des membres de l'association</h1>
+<hr />
+
+	<table border = "1">
+		<tr>
+			<td>id</td>
+			<td>Nom</td>
+			<td>Prénom</td>
+			<td>Adresse Email</td>
+			<td>Admin</td>
+			<td>ChangerStatut</td>
+			<td>Supprimer membre</td>
+
 	<form action='controlMembresAdmin.php' method='post'>	
-	<?php
+<?php
 	while ($tuple = mysql_fetch_array($res))
 	{ 
 		echo"<tr>"
-				."<td>".$tuple['id']."</td>"
-				."<td>".$tuple['nom']."</td>"
-				."<td>".$tuple['prenom']."</td>"
-				."<td>".$tuple['email']."</td>"
-				."<td>".$tuple['admin']."</td>"
-				."<td>"."<input type='checkbox' name='email[]' value='$tuple[email]'/>"."</td>"
-				."<td>"."<input type='checkbox' name='idDelete[]' value='$tuple[id]'/>"."</td>"
+			."<td>".$tuple['id']."</td>"
+			."<td>".$tuple['nom']."</td>"
+			."<td>".$tuple['prenom']."</td>"
+			."<td>".$tuple['email']."</td>"
+			."<td>".$tuple['admin']."</td>"
+			."<td>"."<input type='checkbox' name='email[]' value='$tuple[email]'/>"."<br /></td>" 
+			."<td>"."<input type='checkbox' name='idDelete[]' value='$tuple[id]'/>"."<br /></td>"
 			."</tr>";
 			
 	}
 		echo "</table><br/>";
 	?>
-	<input type ='submit'  value ='Submit' > </form>
-	<a href="./controlAddMembres.php"> Ajouter Membre </a>
+	<input type ='submit'  value ='Submit' > <br /></form>
+	<a href="./controlAddMembres.php">Ajouter Membre</a><br />
