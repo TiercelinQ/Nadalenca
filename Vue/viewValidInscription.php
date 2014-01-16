8d6eaa7e4cde25e8539cb6d5ec37333a388930e8
TiercelinQ <q.tiercelin@gmail.com>
2014-01-16 17:17:34 +0100

    validinscription

--- Vue/viewValidInscription.php
+++ Vue/viewValidInscription.php
@@ -2,14 +2,8 @@
 
 <h1>Inscription réussie</h1>
 <hr />
-<<<<<<< HEAD
-
 	<p>Vous avez réussie à vous inscrire sur le site de l'association Nadalenca. Vous allez recevoir un mail avec vos identifiants (email et mot de passe).</p>
 	<p>Vous allez être rediriger vers la page de connexion.</p>
 	<?php header("Refresh: 3; URL=../Controleur/controlConnexion.php"); ?>
-=======
-	<p>Vous avez réussie à ajouter un nouveau membre.</p>
-	<p>Vous allez être rediriger vers la page de gestion des membres.</p>
-	<?php	header("Refresh: 3; URL=../Controleur/controlMembresAdmin.php"); ?>
->>>>>>> Affichage + Suppression Message recu admin
+
 		
