e4ec7bd47937b57e65a0bc974f54b6eae8f8ccd5
bexchauveto <olivier.bexchauvet@gmail.com>
2014-01-16 14:34:21 +0100

    BDD

--- nadalenca.sql
+++ nadalenca.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Mer 15 Janvier 2014 à 13:58
+-- Généré le: Jeu 16 Janvier 2014 à 13:33
 -- Version du serveur: 5.6.12-log
 -- Version de PHP: 5.4.12
 
@@ -21,6 +21,57 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `nadalenca` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `nadalenca`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `audio`
+--
+
+CREATE TABLE IF NOT EXISTS `audio` (
+  `ida` int(10) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `nbfichier` int(2) NOT NULL,
+  `nomfich1` varchar(255) NOT NULL,
+  `dest1` varchar(255) NOT NULL,
+  `nomfich2` varchar(255) NOT NULL,
+  `dest2` varchar(255) NOT NULL,
+  `nomfich3` varchar(255) NOT NULL,
+  `dest3` varchar(255) NOT NULL,
+  `nomfich4` varchar(255) NOT NULL,
+  `dest4` varchar(255) NOT NULL,
+  `nomfich5` varchar(255) NOT NULL,
+  `dest5` varchar(255) NOT NULL,
+  PRIMARY KEY (`ida`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+
+--
+-- Contenu de la table `audio`
+--
+
+INSERT INTO `audio` (`ida`, `nom`, `nbfichier`, `nomfich1`, `dest1`, `nomfich2`, `dest2`, `nomfich3`, `dest3`, `nomfich4`, `dest4`, `nomfich5`, `dest5`) VALUES
+(1, 'Cecila', 2, '', '', '', '', '', '', '', '', '', ''),
+(2, 'Cecila', 2, '', '', '', '', '', '', '', '', '', ''),
+(3, 'Cecila', 2, '', '', '', '', '', '', '', '', '', ''),
+(4, 'cecila', 1, '', '', '', '', '', '', '', '', '', ''),
+(5, 'cecila', 1, '', '', '', '', '', '', '', '', '', ''),
+(6, 'cecila', 1, '', '', '', '', '', '', '', '', '', ''),
+(7, 'cecila', 1, '', '', '', '', '', '', '', '', '', ''),
+(8, 'cecila', 1, '', '', '', '', '', '', '', '', '', ''),
+(9, 'cecila', 1, 'CECILA-alti.mp3', 'alto', '', '', '', '', '', '', '', ''),
+(10, 'cecila', 1, 'CECILA-alti.mp3', 'alto', '', '', '', '', '', '', '', ''),
+(11, 'cecila', 1, 'CECILA-alti.mp3', 'alto', '', '', '', '', '', '', '', ''),
+(12, 'cecila', 1, 'CECILA-alti.mp3', 'alto', '', '', '', '', '', '', '', ''),
+(13, 'cecila', 1, 'CECILA-alti.mp3', 'alto', '', '', '', '', '', '', '', ''),
+(14, 'cecila', 1, 'CECILA-alti.mp3', 'alto', '', '', '', '', '', '', '', ''),
+(15, 'cecila', 1, 'CECILA-alti.mp3', 'alto', '', '', '', '', '', '', '', ''),
+(16, 'cecila', 1, 'CECILA-alti.mp3', 'alto', '', '', '', '', '', '', '', ''),
+(17, 'cancon', 1, 'CANCONH1.mp3', 'hommes', '', '', '', '', '', '', '', ''),
+(18, 'A Media Nioch', 1, 'a_media_nioch_tenor.mp3', 'Tenor', '', '', '', '', '', '', '', ''),
+(19, 'A Media Nioch', 1, 'a_media_nioch_tenor.mp3', 'Tenor', '', '', '', '', '', '', '', ''),
+(20, 'A Media Nioch', 1, 'a_media_nioch_tenor.mp3', 'Tenor', '', '', '', '', '', '', '', ''),
+(21, 'A Media Nioch', 1, 'a_media_nioch_tenor.mp3', 'Tenor', '', '', '', '', '', '', '', ''),
+(22, 'A Media Nioch', 1, 'a_media_nioch_tenor.mp3', 'Tenor', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -53,28 +104,6 @@ INSERT INTO `concert` (`id`, `titreC`, `heureC`, `minuteC`, `lieuC`, `adresseC`,
 -- --------------------------------------------------------
 
 --
--- Structure de la table `lien`
---
-
-CREATE TABLE IF NOT EXISTS `lien` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `lien` text NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
-
---
--- Contenu de la table `lien`
---
-
-INSERT INTO `lien` (`id`, `lien`, `description`) VALUES
-(1, 'http://google.com', 'Google'),
-(2, 'http://maisondeschoeurs.free.fr/association.htm', 'Association des Chœur de Montpellier');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `message`
 --
 
@@ -87,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `message` (
   `messageM` varchar(255) NOT NULL,
   `dateM` varchar(30) NOT NULL,
   PRIMARY KEY (`idM`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Contenu de la table `message`
@@ -97,10 +126,7 @@ INSERT INTO `message` (`idM`, `nomM`, `prenomM`, `emailM`, `sujetM`, `messageM`,
 (1, 'pochon', 'quentin', 'q.tiercelin@gmail.com', 'test', 'test', '19-12-13'),
 (2, 'tiercelin', 'quentin', 'q.tiercelin@gmail.com', 'test2', 'test2', '19-12-13'),
 (3, 'bexchauvet', 'olivier', 'q.tiercelin@gmail.com', 'test3', 'test', '19-12-13'),
-(4, 'tiercelin', 'quentin', 'q.tiercelin@gmail.com', 'test3', 'test', '19-12-13'),
-(5, 'A', 'D', 'mo@fdfz.fr', 'Test', 'Test', '14-01-14'),
-(6, 'A', 'D', 'mo@fdfz.fr', 'Test', 'test', '14-01-14'),
-(7, 'A', 'D', 'mo@fdfz.fr', 'Test', 'test', '15-01-14');
+(4, 'tiercelin', 'quentin', 'q.tiercelin@gmail.com', 'test3', 'test', '19-12-13');
 
 -- --------------------------------------------------------
 
@@ -109,25 +135,61 @@ INSERT INTO `message` (`idM`, `nomM`, `prenomM`, `emailM`, `sujetM`, `messageM`,
 --
 
 CREATE TABLE IF NOT EXISTS `repertoire` (
-  `idR` int(11) NOT NULL AUTO_INCREMENT,
-  `nomR` varchar(255) NOT NULL,
-  `audioR` varchar(255) NOT NULL,
-  `texteR` varchar(255) NOT NULL,
-  `voix1R` varchar(255) NOT NULL,
-  `voix2R` varchar(255) NOT NULL,
-  `voix3R` varchar(255) NOT NULL,
-  `voix4R` varchar(255) NOT NULL,
-  PRIMARY KEY (`idR`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `idr` int(10) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `idA` int(10) NOT NULL,
+  `idT` int(10) NOT NULL,
+  PRIMARY KEY (`idr`),
+  KEY `idA` (`idA`),
+  KEY `idT` (`idT`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `repertoire`
 --
 
-INSERT INTO `repertoire` (`idR`, `nomR`, `audioR`, `texteR`, `voix1R`, `voix2R`, `voix3R`, `voix4R`) VALUES
-(1, 'Nada let', 'nadaleta', 'nadalett', 'nadaletv1', 'nadaletv2', 'nadaletv3', 'nadaletv4'),
-(2, 'Nadalenca', '', '', '', '', '', ''),
-(3, 'Vert', '', '', '', '', '', '');
+INSERT INTO `repertoire` (`idr`, `nom`, `idA`, `idT`) VALUES
+(2, 'A Media Nioch', 18, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `texte`
+--
+
+CREATE TABLE IF NOT EXISTS `texte` (
+  `idt` int(10) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `nbfich` int(2) NOT NULL,
+  `nomfich1` varchar(255) NOT NULL,
+  `dest1` varchar(255) NOT NULL,
+  `nomfich2` varchar(255) NOT NULL,
+  `dest2` varchar(255) NOT NULL,
+  `nomfich3` varchar(255) NOT NULL,
+  `dest3` varchar(255) NOT NULL,
+  `nomfich4` varchar(255) NOT NULL,
+  `dest4` varchar(255) NOT NULL,
+  `nomfich5` varchar(255) NOT NULL,
+  `dest5` varchar(255) NOT NULL,
+  PRIMARY KEY (`idt`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+--
+-- Contenu de la table `texte`
+--
+
+INSERT INTO `texte` (`idt`, `nom`, `nbfich`, `nomfich1`, `dest1`, `nomfich2`, `dest2`, `nomfich3`, `dest3`, `nomfich4`, `dest4`, `nomfich5`, `dest5`) VALUES
+(1, 'cecila', 1, 'Cecila.pdf', 'partition', '', '', '', '', '', '', '', ''),
+(2, 'cecila', 1, 'Cecila.pdf', 'partition', '', '', '', '', '', '', '', ''),
+(3, 'cecila', 1, 'Cecila.pdf', 'partition', '', '', '', '', '', '', '', ''),
+(4, 'cecila', 1, 'Cecila.pdf', 'partition', '', '', '', '', '', '', '', ''),
+(5, 'cecila', 1, 'Cecila.pdf', 'partition', '', '', '', '', '', '', '', ''),
+(6, 'cancon', 1, 'CanÃ§on.pdf', 'paroles', '', '', '', '', '', '', '', ''),
+(7, 'A Media Nioch', 1, 'A_mieja_nioch.pdf', 'Partition', '', '', '', '', '', '', '', ''),
+(8, 'A Media Nioch', 1, 'A_mieja_nioch.pdf', 'Partition', '', '', '', '', '', '', '', ''),
+(9, 'A Media Nioch', 1, 'A_mieja_nioch.pdf', 'Partition', '', '', '', '', '', '', '', ''),
+(10, 'A Media Nioch', 1, 'A_mieja_nioch.pdf', 'Partition', '', '', '', '', '', '', '', ''),
+(11, 'A Media Nioch', 1, 'A_mieja_nioch.pdf', 'Partition', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -151,23 +213,27 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `voix` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Contenu de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `email`, `mdp`, `statut`, `admin`, `adresseP`, `codeP`, `ville`, `numtelF`, `numtelM`, `voix`) VALUES
-(1, 'Tiercelin', 'Quentin', 'q.tiercelin@gmail.com', 'licorne', 'Membre du conseil d''administration', 0, '20, avenue du Docteur Pezet', '35620', 'Montpellier', '04.99.23.93.98', '06.50.80.66.77', 'voix3'),
-(2, 'Beule', 'JÃ©rÃ©my', 'androalphas@hotmail.fr', 'licorne', '', 1, '', '', '', '', '', 'voix'),
-(3, 'Bex-Chauvet', 'Olivier', 'fullofwrath@hotmail.fr', 'lilo', 'PrÃ©sident', 1, '114 rue lol', '34090', 'Montpellier', '04.22.33.44.55', '06.22.33.44.55', 'voix2'),
-(4, 'Pochon', 'Amaury', 'momoamo@hotmail.fr', 'test', '', 1, '', '', '', '', '', 'voix'),
-(5, 'Pochona', '1', 'celestine.pochon@hotmail.fr', 'test', '', 0, '', '', '', '', '', 'voix'),
-(6, 'Pochon', 'Amaury', 'momoam@hotmail.fr', 'test', '', 0, '', '', '', '', '', 'voix'),
-(7, 'Amauy', 'Pochon', 'momoa@hotmail.fr', 'test', '', 0, '', '', '', '', '', 'voix'),
-(8, '1', '2', 'test@gmail.com', 'test', '', 0, '', '', '', '', '', 'voix'),
-(9, 'test', 'ettd', 'lolilol@yopmail.com', 'test', '', 0, '', '', '', '', '', 'voix'),
-(10, 'test', 'test', 'de@de.fr', 'lol', '', 0, '', '', '', '', '', 'voix');
+(1, 'Tiercelin', 'Quentin', 'q.tiercelin@gmail.com', 'licorne', '', 1, '20, avenue du Docteur Pezet', '34090', 'Montpellier', '04.99.23.93.98', '06.50.80.66.77', 'voix'),
+(2, 'Beule', 'JÃ©rÃ©my', 'androalphas@hotmail.fr', 'licorne', '', 0, '', '', '', '', '', 'voix'),
+(3, 'Bex-Chauvet', 'Olivier', 'fullofwrath@hotmail.fr', 'lilo', 'PrÃ©sident', 1, '114 rue du ponay', '34090', 'Montpellier', '04.22.33.44.55', '06.22.33.44.55', 'voix2');
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `repertoire`
+--
+ALTER TABLE `repertoire`
+  ADD CONSTRAINT `repertoire_ibfk_1` FOREIGN KEY (`idA`) REFERENCES `audio` (`ida`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `repertoire_ibfk_2` FOREIGN KEY (`idT`) REFERENCES `texte` (`idt`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
