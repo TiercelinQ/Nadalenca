a6800748e6f867b6ef2844e5162aa49ab6dbf167
TiercelinQ <q.tiercelin@gmail.com>
2014-01-16 16:15:00 +0100

    view redirection inscription et ajout membre

--- Vue/viewAccueil.php
+++ Vue/viewAccueil.php
@@ -1,7 +1,7 @@
 	<h1>Bienvenue</h1>
 	<hr />
 	
-		<img src="../Images/LogoMaquette.png" alt="nadalenca" title="Association Nadalenca"/>
+		<img src="../Images/LogoMaquette1.png" alt="nadalenca" title="Association Nadalenca"/>
 
 	<h3>Bienvenue sur le site de l'Association du Choeur Polyphonique Occitan NADALENCA de Montpellier</h3>
 
