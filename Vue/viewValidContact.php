0f413800356a867c7ad0b2a2318644c81fa1a843
TiercelinQ <q.tiercelin@gmail.com>
2014-01-16 22:30:43 +0100

    Design viewValidContact avec redirection

--- Vue/viewValidContact.php
+++ Vue/viewValidContact.php
@@ -1,4 +1,4 @@
-	<h2>Envoi du message réussi</h2>
+	<h1>Envoi du message réussi</h1>
 	<hr />
 	<p>Votre message a bien été transmis à l'administrateur.</p>
-
+	<?php header("Refresh: 3; URL=../Controleur/controlContact.php"); ?>
