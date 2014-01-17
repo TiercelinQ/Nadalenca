c1e5c57ce30f6d02f2f36fa44899dd22c8ca324d
ivori <ivori.koyot@gmail.com>
2014-01-17 15:35:10 +0100

    controlAddProduct image beulej

--- Controleur/controlAddProduc.php
+++ Controleur/controlAddProduc.php
@@ -12,6 +12,43 @@ include("./menu.php");
 	$production = new production($idP, $nomP, $descriptionP, $prixP, $imageP);
 	$production->create();
 
+	$dossier = '../Images/';
+	//$fichier = basename($_FILES['image']['name']);
+	$taille_maxi = 1000000;
+	$taille = filesize($_FILES['image']['tmp_name']);
+	$extensions = array('.png', '.gif', '.jpg', '.jpeg');
+	$extension = strrchr($_FILES['image']['name'], '.'); 
+	//Début des vérifications de sécurité...
+	if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
+	{
+		 $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
+	}
+	if($taille>$taille_maxi)
+	{
+		 $erreur = 'Le fichier est trop gros...';
+	}
+	if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+	{
+		 //recupere l'extension du fichier
+		 $extension_upload = strtolower(  substr(  strrchr($_FILES['image']['name'], '.')  ,1)  );
+		 //On formate le nom du fichier ici...
+		 $nom = md5(uniqid(rand(), true));
+		 if(move_uploaded_file($_FILES['image']['tmp_name'], $dossier . $nom .'.'.$extension_upload)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+		 {
+			  echo 'Upload effectué avec succès !';
+		 }
+		 else //Sinon (la fonction renvoie FALSE).
+		 {
+			  echo 'Echec de l\'upload !';
+		 }
+	}
+	else
+	{
+		 echo $erreur;
+	} 
+
+	include("./footer.php");
+
 	header('Location: ./controlProductionAdmin.php');
 
 ?>
