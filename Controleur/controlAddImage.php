1533d33f993b2428d291f5c46f0f9dfb36706693
ivori <ivori.koyot@gmail.com>
2014-01-17 11:48:55 +0100

    Uplouade d'image beulej

---
+++ Controleur/controlAddImage.php
@@ -0,0 +1,16 @@
+<?php include("config.php") ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Concerts Administrateur</title>
+			<meta charset="utf-8" />
+			<link rel="stylesheet" href="../style.css" />
+	</head>
+	
+<?php 
+include("./menu.php");
+
+
+include("../Vue/viewAddImage.php");
+include("./footer.php");
+?>
--- Vue/viewAddImage.php
+++ Vue/viewAddImage.php
@@ -1,10 +1,10 @@
-<form method="post" action="reception.php" enctype="multipart/form-data">
+<form method="post" action="controlValidAddIimage.php" enctype="multipart/form-data">
      <label for="image">Image (JPG, PNG ou GIF) :</label><br />
-     <input type="file" name="icone" id="icone" /><br />
-	 <input type="hidden" name="MAX_FILE_SIZE" value="104857600" />
+     <input type="file" name="image" id="image" /><br />
+	 <input type="hidden" name="maxsize" value="1048576000000" />
      <label for="titre">Titre du fichier (max. 50 caractères) :</label><br />
      <input type="text" name="titre" value="Titre du fichier" id="titre" /><br />
      <label for="description">Description de votre fichier (max. 255 caractères) :</label><br />
      <textarea name="description" id="description"></textarea><br />
-     <input type="submit" name="submit" value="Envoyer" />
+     <input type="submit" name="submit" value="Envoyer" />  
 </form>
