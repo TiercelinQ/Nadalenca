e26cf38ad38ce871774145c8f3111dde466711be
pochona <momoamo@hotmail.fr>
2014-01-17 10:18:32 +0100

    Correction modif statut membres voir détails
    Changement de l'ordre du l'inclusion pour eviter le beug d'inclusion du
    modèle utilisateur + ajout de redirection en cas de modification pour
    actualiser les pages correctement

--- Controleur/controlMembresAdmin.php
+++ Controleur/controlMembresAdmin.php
@@ -1,12 +1,15 @@
 <?php 
 //include ("../Modele/Utilisateur.php");
 //include("./menu.php");
+
+include("./controlMembres.php");
 if(isset($_POST['email'])) //sera vrai si au moins un moins un checkbox a Ã©tÃ© cochÃ©
 	{
 	foreach($_POST['email'] as $email) //ici Ã  chaque passage $admin contiendra la valeur de l'attribut value d'une des cases Ã  cocher
 		{ 
 		Utilisateur::changerAdmin($email);
 		}
+		header("Location: ./controlMembresAdmin.php");
 	}
 	
 if(isset($_POST['idDelete'])) //sera vrai si au moins un moins un checkbox a Ã©tÃ© cochÃ©
@@ -15,7 +18,6 @@ if(isset($_POST['idDelete'])) //sera vrai si au moins un moins un checkbox a Ã�
 		{ 
 		Utilisateur::delete($idDelete);
 		}	
+		header("Location: ./controlMembresAdmin.php");
 	}
-include("./ControlMembres.php");
-
 ?>
