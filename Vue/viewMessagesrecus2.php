4be73ba75950c6c0b0678e82016b57760b62e746
TiercelinQ <q.tiercelin@gmail.com>
2014-01-17 13:10:31 +0100

    CSS admin message reçu

--- Vue/viewMessagesrecus2.php
+++ Vue/viewMessagesrecus2.php
@@ -5,22 +5,22 @@
 include("../Modele/Message.php");
 	while ($tuple = mysql_fetch_array($res))
 	{ ?>	
-		<div class="container">
-			<div class="gauche">
-				<?php echo ".$tuple[sujetM]."; ?>
+		<div class="container_message">
+			<div class="gauche_message">
+				<p>SUJET : <?php echo "$tuple[sujetM]"; ?></p>
 			</div>
-			<div class="milieu">
-				<?php echo ".$tuple[nomM]."; ?>
+			<div class="milieu_message">
+				<p>NOM : <?php echo "$tuple[nomM] $tuple[prenomM]"; ?></p>
 			</div>
-			<div class="droite">
-				<?php echo ".$tuple[prenomM]."; ?>
+			<div class="droite_message">
+				<p>EMAIL : <?php echo "$tuple[emailM]"; ?></p>
 			</div>
-			<div class="dessous">
-				<?php echo ".$tuple[messageM]."; ?>
+			<div class="dessous_message">
+				<p>MESSAGE : <?php echo "$tuple[messageM]"; ?></p>
 			</div>
-			<div class="suppr">
+			<div class="suppr_message">
 			<?php echo"<button type= \"button\" " . "onclick=\"location.href='./controlSupMessage.php?idM=" 
-		. $tuple['idM']. "'\"><img src='../Images/remove.png'  alt='Image Suppression Message'/>
+		. $tuple['idM']. "'\"><img src='../Images/remove.png'  title='Suppression' alt='Image Suppression Message'/>
         </button>";?>
 			</div>
 		</div>
@@ -29,5 +29,5 @@ include("../Modele/Message.php");
 <?php
 	} 
 		?>
-
+	<br />
 	<a href="../Controleur/controlAdministrateur.php">Retour au panneau d'administration</a>
