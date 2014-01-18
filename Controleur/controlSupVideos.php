5c019b6e368521a5f0b2c340debb8eb666475433
TiercelinQ <q.tiercelin@gmail.com>
2014-01-18 21:01:44 +0100

    Mise en place des commentaires 4

--- Controleur/controlSupVideos.php
+++ Controleur/controlSupVideos.php
@@ -1,11 +1,24 @@
 <?php
-include ("../Modele/Video.php");
-if(isset($_GET['idV'])){
-	$idV = $_GET['idV'];
-	if(Video::existId($idV)){
-		Video::delete($idV);
+	//On récupère la Class Vidéo
+	include ("../Modele/Video.php");
+
+	//Vérification de ce qui a été récupéré dans idV
+	if(isset($_GET['idV']))
+	{
+		//la valeur $idV prend le contenu qui a été récupéré	
+		$idV = $_GET['idV'];
+		//Vérification si la video existe par son idV
+		if(Video::existId($idV))
+		{
+			//Création d'un constructeur en rapport à la fonction delete
+			Video::delete($idV);
+			//Redirection vers la page des videos de la session admin
+			header("Location:./controlVideosAdmin.php");
+		}
+	}
+	else
+	{
+		//Redirection vers la page des videos de la session admin
 		header("Location:./controlVideosAdmin.php");
 	}
-}
-else  header("Location:./controlVideosAdmin.php");
 ?>
