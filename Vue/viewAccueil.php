669dc92600c411118cc651e1d1c7c1b89c85bcc4
TiercelinQ <q.tiercelin@gmail.com>
2014-01-20 20:38:52 +0100

    phrase accueil centrée

--- Vue/viewAccueil.php
+++ Vue/viewAccueil.php
@@ -3,6 +3,7 @@
 	
 		<img src="../Images/LogoMaquette1.png" alt="nadalenca" title="Association Nadalenca"/>
 
+	<span id="accueil">
 	<h3>Bienvenue sur le site de l'Association du Choeur Polyphonique Occitan NADALENCA de Montpellier</h3>
-
+	</span>
 
