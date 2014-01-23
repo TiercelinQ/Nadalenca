a2fbeffe91a972ecbec62292a1bb6571bf7027f1
ivori <ivori.koyot@gmail.com>
2014-01-23 15:02:00 +0100

    Affichage noel OK beulej

---
+++ Vue/viewNoel.php
@@ -0,0 +1,43 @@
+<h1>Noel Occitan</h1>
+<hr />
+
+Depuis 1990, chaque 24 Décembre, Nadalenca organise le Noël Occitan de Montpellier 
+
+(Veillée traditionnelle et Messe de Minuit en langue d’oc) qui est devenu une véritable 
+
+La veillée traditionnelle autour de la crèche permet d’attendre la Messe de Minuit et propose 
+
+des contes, textes en Occitan, et bien sur les fameux « Nadalets » traditionnels. A Minuit 
+
+célébration de la Messe de Noël. Tout est en Occitan, les textes des chants, de l·homélie et 
+
+de la messe sont traduits dans les livrets mis à disposition pour tous les participants.
+
+n Eglise ST Mathieu (1990 – 1993 – 1994 à 1996 – 1999 à 2003)
+
+n Chapelle du Collège de L’assomption (1997 – 1998) 
+
+n Chapelle des pénitents blanc (2004 -2005)
+
+n Maison des chœurs (2006 à 2013 )
+
+<ul>
+					
+	<?php
+	while ($tuple = mysql_fetch_array($res))
+	{ 
+
+		echo "<li> $tuple[lieuN]" ." (" .  $tuple['dateDebN']."-" .$tuple['dateFinN'].")"."</li>";
+	}
+
+		
+	?>
+</ul>
+	
+La veillée traditionnelle autour de la crèche permet d’attendre la Messe de Minuit et propose 
+
+des contes, textes en Occitan, et bien sur les fameux « Nadalets » traditionnels. A Minuit 
+
+célébration de la Messe de Noël. Tout est en Occitan, les textes des chants, de l’homélie et 
+
+de la messe sont traduits dans les livrets mis à disposition pour tous les participants.
