d8e95f189ccd37bc6014723abc09c53f14f2ca97
pochona <momoamo@hotmail.fr>
2014-01-23 16:06:56 +0100

    MVC Recrutement FINI

--- Vue/viewAdministration.php
+++ Vue/viewAdministration.php
@@ -6,7 +6,8 @@
 		<li><a href="../Controleur/controlAddImage.php">Gestion des images</a></li>
 		<li><a href="../Controleur/controlMembresAdmin.php">Gestion des membres</a></li>
 		<li><a href="../Controleur/controlMessagesrecus.php">Gestion des messages reçus</a></li>
-		<li><a href="../Controleur/controlLiensAdmin.php">Gestion des liens</a></li>		
+		<li><a href="../Controleur/controlLiensAdmin.php">Gestion des liens</a></li>
+		<li><a href="../Controleur/controlRecrutAdmin.php">Gestion des recrutements</a>		
 		<li><a href="../Controleur/controlVideosAdmin.php">Gestion des vidéos</a>
 		<li><a href="../Controleur/controlMDPAdmin.php">Gestion du mot de passe d'inscription</a></li>
 		<li><a href="../Controleur/controlRepAdmin.php">Gestion du répertoire</a></li>
