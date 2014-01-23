af16ee17329fbccd1abd8089d055990ca3645b17
TiercelinQ <q.tiercelin@gmail.com>
2014-01-23 15:45:40 +0100

    redim image nada

--- Vue/viewAccueilOcc.php
+++ Vue/viewAccueilOcc.php
@@ -4,12 +4,13 @@
 		<img src="../Images/LogoMaquette1.png" alt="nadalenca" title="Association Nadalenca"/>
 
 	<span id="accueil">
-	<h3>« Nadalenca » es lo còr polifonic occitan de Montpelhièr.</h3>
-
+		<h3>« Nadalenca » es lo còr polifonic occitan de Montpelhièr</h3>
+	</span>
+	
 	<p>Compausat d’un trentenat de coristast, lo còr interpreta d’òbras polifonicas sortidas del repertòri occitan : Nadalets e cants tradicionals e 
 	tanben d’òbras classicas adaptadas en occitan.</p>
 
 	<p>Nadalenca participa a la promocion del patrimòni cultural occitan amb de concèrts balhats dins Occitània tota e enlà, e encara amb d’escambis 
 	regulars amb d’autras lengas romanicas mediterranèas.<p>
-	</span>
+	
 
