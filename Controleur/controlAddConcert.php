b33371e15f62ba1f239104ccf8a9f984870a03bb
ivori <ivori.koyot@gmail.com>
2014-01-24 11:35:42 +0100

    Commentaire CORRIGE beulej

--- Controleur/controlAddConcert.php
+++ Controleur/controlAddConcert.php
@@ -25,31 +25,31 @@
 
 
 <?php 
-//On inclut le menu dans le controleur d'ajout d'un concert
+//On inclut le menu dans le controleur d'ajout d'un noel occitan
 include("./menu.php");
 
 	if(isset($_SESSION['login']) && isset($_SESSION['admin']) && $_SESSION['admin'] != 0)
 	{
 
-	//Requête SQL permettant d'afficher les concerts du plus vieux au plus récents
+	//Requête SQL permettant d'afficher les noels occitan du plus vieux au plus récents
 	$req="SELECT * from Concert order by dateC ASC ";
 	//On exécute la requête
 	$restot =mysql_query($req);
 
 
-		//On inclut la vue de la gestion des concerts en session administrateur
+		//On inclut la vue de la gestion des noels occitan en session administrateur
 	include("../Vue/viewConcertAdmin.php");
-	//On inclut la vue de l'ajout d'un concert en session administrateur
+	//On inclut la vue de l'ajout d'un noel occitan en session administrateur
 	include("../Vue/viewAddConcert.php");
 	}
 	else
 	{ 		
-	//Requête permettant d'afficher les concerts de la dateC jusqu'à la date de maintenant
+	//Requête permettant d'afficher les noels occitans de la dateC jusqu'à la date de maintenant
 	$req="SELECT * from concert where dateC>now() order by dateC ASC ";
 	//On execute la requête
 	$res1 =mysql_query($req);
 	
-	//Requête permettant d'afficher les concerts de la date de maintenant jusqu'à la dateC
+	//Requête permettant d'afficher les noel occitan de la date de maintenant jusqu'à la dateC
 	$req="SELECT * from concert where dateC<now() order by dateC ASC ";
 	//On execute la requête
 	$res2 =mysql_query($req);
--- Controleur/controlSupNoel.php
+++ Controleur/controlSupNoel.php
@@ -7,23 +7,23 @@
 	{
 		//la valeur $id prend ce qui a été récupéré
 		$idN = $_GET['idN'];
-		//Vérification l'existence du concert avec l'appel de la fonction existId de la classe Concert
+		//Vérification l'existence d'un noel occitan avec l'appel de la fonction existId de la classe noel
 		if(Noel::existId($idN))
 		{
-			//Appel de la fonction delete de la classe Concert
+			//Appel de la fonction delete de la classe noel
 			Noel::delete($idN);
-			//Redirection vers la page d'ajout d'un concert
+			//Redirection vers la page d'ajout d'un noel occitan
 			header("Location:./controlAddNoel.php");
 		}
 		else 
 		{
-			//Sinon redirection vers la page d'ajout d'un concert
+			//Sinon redirection vers la page d'ajout d'un noel occitan
 			header("Location:./controlAddNoel.php");
 		}
 	}
 	else 
 	{
-		//Sinon redirection vers la pahe d'ajout d'un concert
+		//Sinon redirection vers la pahe d'ajout d'un noel occitan
 		header("Location:./controlAddNoel.php");
 	}
 ?>
