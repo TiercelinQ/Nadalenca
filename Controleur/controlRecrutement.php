3ab27dbecc944acfba23fcd9fea080f7a7f3415a
bexchauveto <olivier.bexchauvet@gmail.com>
2014-01-24 13:47:20 +0100

    Correction

--- Controleur/controlRecrutement.php
+++ Controleur/controlRecrutement.php
@@ -1,5 +1,3 @@
-<!-- On inclut le fichier config pour accéder à la base de données-->
-<?php include("./config.php");?>
 <!DOCTYPE html>
 <html>
    <head>
@@ -10,12 +8,12 @@
    </head>
    
 <?php 
-  //On inclut le fichier config pour accéder à la base de données
-  include("config.php")
+	//On inclut le fichier config pour accéder à la base de données
+	include("config.php");
 	//On inclut le menu en fonction de la session en cours
 	include("./menu.php");
 	//On inclut la vue d'affichahe pour contacter l'administrateur 
-	include("../vue/viewRecrutement.php");
+	include("../Vue/viewRecrutement.php");
     //On inclut le footer
 	include("./footer.php");
 
