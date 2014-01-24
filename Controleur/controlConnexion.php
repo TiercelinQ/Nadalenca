61d0c25b35b8a9155cce8372111281926ddb3be2
ivori <ivori.koyot@gmail.com>
2014-01-24 13:16:01 +0100

    w3c 2

--- Controleur/controlConnexion.php
+++ Controleur/controlConnexion.php
@@ -1,5 +1,3 @@
-<!-- On inclut le fichier config pour accéder à la base de données-->
-<?php include("config.php") ?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -8,6 +6,8 @@
 			<link rel="stylesheet" href="../style.css" />
 	</head>
 <?php 
+//On inclut le fichier config pour accéder à la base de données
+include("config.php")
 //On inclut le menu en fonction de la session en cours
 include("./menu.php");
 //On inclut la vue d'affichage pour se connecter
